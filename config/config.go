6306a786755d568396218758e57637fe80fa347f
Babatunde Ololade <babatundeololade50@gmail.com>
2024-06-09 11:31:32 +0300

    add default config

--- config/config.go
+++ config/config.go
@@ -8,23 +8,28 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Token represents token-related configuration.
 type Token struct {
 	FileName   string `mapstructure:"FileName"`
 	AppName    string `mapstructure:"AppName"`
 	FolderName string `mapstructure:"FolderName"`
 }
 
+// Service represents a service configuration.
 type Service struct {
 	Host string `mapstructure:"host"`
 }
 
+// Config holds the entire configuration structure.
 type Config struct {
 	Token    Token              `mapstructure:"token"`
 	Services map[string]Service `mapstructure:"services"`
 }
 
+// C is the global configuration variable.
 var C Config
 
+// LoadConfig loads the configuration from config.yaml.
 func LoadConfig() {
 	exePath, err := os.Executable()
 	if err != nil {
@@ -38,19 +43,18 @@ func LoadConfig() {
 	// Check if the config file exists
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		// If the file does not exist, create it with default values
-		defaultConfig := []byte(`
-	token:
-	FileName: "token"
-	AppName: "gitversity"
-	FolderName: ".config"
+		defaultConfig := []byte(`token:
+  FileName: "token"
+  AppName: "gitversity"
+  FolderName: ".config"
 
-	services:
-	user_grpc:
-		host: "grpc.dev.user.api.gitversity.com:443"
-	assignment_grpc:
-		host: "grpc.dev.assignment.api.gitversity.com:443"
-	git_grpc:
-		host: "grpc.dev.git.api.gitversity.com:443"
+services:
+  user_grpc:
+    host: "grpc.dev.user.api.gitversity.com:443"
+  assignment_grpc:
+    host: "grpc.dev.assignment.api.gitversity.com:443"
+  git_grpc:
+    host: "grpc.dev.git.api.gitversity.com:443"
 `)
 
 		err = os.WriteFile(configPath, defaultConfig, 0644)
